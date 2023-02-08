d2643f2b27b5689039455b812c9793cb43130a8b
Yiru <rosa60721@gmail.com>
2023-02-08 21:46:38 +0800

    git init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{data}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT)
+    const url = import.meta.env.VITE_PATH;
+    this.$http.get('https://randomuser.me/api/')
+    .then((res) => {
+        console.log(res.data);
+        this.data = res.data.results[0];
+      })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
